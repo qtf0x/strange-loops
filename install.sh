90f84b905c7dba0906b0e4366493de5175c1526c
Aster Marias <aster@slware.org>
2026-07-30 12:46:26 -0600

    Wrote script to generate empty tree on install

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p app/apis/ components/ dump/fixtures/ lib/native/linux-x86 storage/ log/ test/mocks/development/ test/mocks/test/ tmp/
+
+touch log/production.log
+touch tmp/restart.txt
+
+bundle config set path 'vendor/bundle'
+bundle install
